afca7ddd3e8d91294a7d2a1c4d6480374fce73b6
Tim Henkes <tim.henkes@e-mundo.de>
2020-10-24 11:36:54 +0200

    Started reworking the file structure into a standalone Python package

---
+++ setup.py
@@ -0,0 +1,63 @@
+# pylint: disable=exec-used
+import os
+
+from setuptools import setup, find_packages
+
+source_root = os.path.join(os.path.dirname(os.path.abspath(__file__)), "scapy_packet_viewer")
+
+version_scope = {}
+with open(os.path.join(source_root, "version.py")) as f:
+    exec(f.read(), version_scope)
+version = version_scope["__version__"]
+
+project_scope = {}
+with open(os.path.join(source_root, "project.py")) as f:
+    exec(f.read(), project_scope)
+project = project_scope["project"]
+
+with open("README.md") as f:
+    long_description = f.read()
+
+classifiers = [ # TODO
+    "Intended Audience :: Developers",
+    "Intended Audience :: Education",
+    "Intended Audience :: Information Technology",
+    "Intended Audience :: Science/Research",
+
+    "License :: OSI Approved :: Apache Software License",
+
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8"
+]
+
+classifiers.extend(project["categories"])
+
+if version["tag"] == "alpha":
+    classifiers.append("Development Status :: 3 - Alpha")
+
+if version["tag"] == "beta":
+    classifiers.append("Development Status :: 4 - Beta")
+
+if version["tag"] == "stable":
+    classifiers.append("Development Status :: 5 - Production/Stable")
+
+del project["categories"]
+del project["year"]
+
+setup(
+    version = version["short"],
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    license = "Apache 2.0", # TODO
+    packages = find_packages(),
+    install_requires = [ # TODO
+    ],
+    python_requires = ">=3.4",
+    zip_safe = False,
+    classifiers = classifiers,
+    **project
+)
